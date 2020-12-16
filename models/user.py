5d9929d456ca7f2bb0a85147e339208548e30bc2
oumaymabg <oumaymabou257@gmail.com>
2020-12-16 18:27:44 +0000

    user

--- models/user.py
+++ models/user.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class User(BaseModel):
