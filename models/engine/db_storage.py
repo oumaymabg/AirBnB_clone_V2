9025b5d089a2bc9c3dfd4e918c123d561afbd963
oumaymabg <oumaymabou257@gmail.com>
2020-12-17 20:13:48 +0000

    db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -72,4 +72,4 @@ class DBStorage:
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
-        self.__session = Session()
+        self.__session = Session
