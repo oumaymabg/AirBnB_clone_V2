0ae41d9bf335613ccd3646ef74e6d3573d65b36f
oumaymabg <oumaymabou257@gmail.com>
2020-12-17 21:33:47 +0000

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -6,52 +6,52 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
 
 
-Base = declarative_base()
+Base = declarative_base(
 
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    id=Column(String(60), primary_key=True, nullable=False)
+    created_at=Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at=Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.id=str(uuid4())
+        self.created_at=datetime.now()
+        self.updated_at=datetime.now()
         if args is not ():
             self.__dict__.update(args[0])
         if (kwargs is not None):
             for key, value in kwargs.items():
                 if (key == "created_at" or key == "updated_at"):
                     if (type(value) is str):
-                        Date_obj = datetime.strptime(
+                        Date_obj=datetime.strptime(
                             value, '%Y-%m-%dT%H:%M:%S.%f')
-                        self.__dict__[key] = Date_obj
+                        self.__dict__[key]=Date_obj
                 else:
-                    self.__dict__[key] = value
+                    self.__dict__[key]=value
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        cls=(str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
-        self.updated_at = datetime.now()
+        self.updated_at=datetime.now()
         storage.new(self)
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
+        dictionary={}
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary['created_at']=self.created_at.isoformat()
+        dictionary['updated_at']=self.updated_at.isoformat()
         if "_sa_instance_state" in dictionary:
             del dictionary["_sa_instance_state"]
         return dictionary
