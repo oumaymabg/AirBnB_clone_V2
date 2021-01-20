21a96d6ca09eab045e6ef105d514675ac9629cb7
oumaymabg <oumaymabou257@gmail.com>
2021-01-20 16:49:49 +0000

    setup_web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
-# Write a Bash script that sets up your web servers for the deployment of web_static
+# Task 0 : Prepare your web servers
 sudo apt-get -y update
-sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-sudo echo  "<html>
+sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
+sudo rm -f -R /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-location="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
-sudo service nginx start
+TEST_STRING="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
+sudo sed -i "53i $TEST_STRING" /etc/nginx/sites-available/default
+sudo service nginx restart
